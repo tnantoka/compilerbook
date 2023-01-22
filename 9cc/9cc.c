c000adf9a2169e45e8761294a658c41259f13ec1
tnantoka <tnantoka@bornneet.com>
2023-01-22 14:36:54 +0900

    feat: unary operator

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -129,6 +129,7 @@ struct Node {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
@@ -146,6 +147,7 @@ Node *new_node_num(int val) {
   return node;
 }
 
+// expr    = mul ("+" mul | "-" mul)*
 Node *expr() {
   Node *node = mul();
 
@@ -159,19 +161,30 @@ Node *expr() {
   }
 }
 
+// mul     = unary ("*" unary | "/" unary)*
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   for (;;) {
     if (consume('*'))
-      node = new_node(ND_MUL, node, primary());
+      node = new_node(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_node(ND_DIV, node, primary());
+      node = new_node(ND_DIV, node, unary());
     else
       return node;
   }
 }
 
+// unary   = ("+" | "-")? primary
+Node *unary() {
+  if (consume('+'))
+    return primary();
+  if (consume('-'))
+    return new_node(ND_SUB, new_node_num(0), primary());
+  return primary();
+}
+
+// primary = num | "(" expr ")"
 Node *primary() {
   if (consume('(')) {
     Node *node = expr();
